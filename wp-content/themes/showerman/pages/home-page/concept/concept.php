b7815edabf30af8441d8894cee01529580c11459
Lea <leamachyleger@gmail.com>
2024-01-30 21:50:41 +0100

    refont bloc concept

--- wp-content/themes/showerman/pages/home-page/concept/concept.php
+++ wp-content/themes/showerman/pages/home-page/concept/concept.php
@@ -17,26 +17,26 @@ endif;
 
     <div class="section container-fluid concept bg-yellow">
         <div class="row">
-            <div class="col-lg-6 col-xl-6 col-xs-12 p-0">
+            <div class="col-lg-12 col-xl-12 col-xs-12 p-0 ">
                 <?php
                 // Affiche l'image si celle-ci est définie.
                 if ($image) {
-                    echo '<div style="background-image: url(\'' . esc_url($image['url']) . '\');" class="bg-image"></div>';
+                    echo '<div style="background-image: url(\'' . esc_url($image['url']) . '\'); height: 430px;" class="bg-image"></div>';
                 }
                 ?>
-            </div>
-            <div class="col-lg-6 col-xl-6 col-xs-12 p-5">
-                <?php
-                // Affiche le titre si celui-ci est défini.
-                if ($titre) {
-                    echo '<h2 data-aos="fade-up">' . ($titre) . '</h2>';
-                }
+                <div class="col-lg-6 col-xl-6 col-xs-12 p-5 text-on-picture">
+		            <?php
+		            // Affiche le titre si celui-ci est défini.
+		            if ($titre) {
+			            echo '<h2 data-aos="fade-up" class="pt-3">' . ($titre) . '</h2>';
+		            }
 
-                // Affiche le texte si celui-ci est défini.
-                if ($texte) {
-                    echo '<div data-aos="fade-up">' . ($texte) . '</div>';
-                }
-                ?>
+		            // Affiche le texte si celui-ci est défini.
+		            if ($texte) {
+			            echo '<div data-aos="fade-up"><p>' . ($texte) . '</p></div>';
+		            }
+		            ?>
+                </div>
             </div>
         </div>
     </div>
