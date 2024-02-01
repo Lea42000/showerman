a1e06d8821e26b8ba926a3d75eeb90e9613236d3
aphamani <antoine.phamanivanh@epitech.eu>
2024-02-01 17:59:19 +0900

    fix(header) fix header.js

--- wp-content/themes/showerman/menu/header/header.js
+++ wp-content/themes/showerman/menu/header/header.js
@@ -26,37 +26,23 @@
 window.addEventListener('scroll', function () {
     var header = document.getElementById('header');
     var videoSection = document.querySelector('.video-home');
-    var menuLinks = document.querySelectorAll('.main-navbar a'); // Select menu links
-    var navbarLinks = document.querySelectorAll('.header-nav a'); // Select navbar links
-
-    // Get the bottom offset of the video section
+    var menuLinks = document.querySelectorAll('.main-navbar a');
+    var navbarLinks = document.querySelectorAll('.header-nav a');
     var videoBottom = videoSection.offsetTop + videoSection.offsetHeight;
-
-    // Get the scroll position
     var scrollPosition = window.scrollY || window.pageYOffset;
-
-    // Check if scroll position is greater than or equal to the bottom offset of the video section
     if (scrollPosition >= videoBottom) {
-        header.classList.add('dark'); // Add dark class to header
-
-        // Change color of menu links to black
+        header.classList.add('dark');
         menuLinks.forEach(function (link) {
             link.style.color = 'black';
         });
-
-        // Change color of navbar links to black
         navbarLinks.forEach(function (link) {
             link.style.color = 'black';
         });
     } else {
-        header.classList.remove('dark'); // Remove dark class from header
-
-        // Change color of menu links to white
+        header.classList.remove('dark');
         menuLinks.forEach(function (link) {
             link.style.color = 'white';
         });
-
-        // Change color of navbar links to white
         navbarLinks.forEach(function (link) {
             link.style.color = 'white';
         });
